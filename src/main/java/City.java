d622012933c38813f6a33b851f026d843f74c33c
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2023-03-15 23:02:16 +0300

    Hibernate+City

--- src/main/java/City.java
+++ src/main/java/City.java
@@ -26,19 +26,6 @@ public class City {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        City city = (City) o;
-        return Objects.equals(cityName, city.cityName) && Objects.equals(cityId, city.cityId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(cityName, cityId);
-    }
-
-    @Override
     public String toString() {
         return "City " + cityName + " cityId " + cityId;
     }
