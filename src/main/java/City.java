a530aee69db7d545e500f6548d91f976e8015912
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2023-03-16 16:20:46 +0300

    Hibernate+City+Entity

--- src/main/java/City.java
+++ src/main/java/City.java
@@ -1,12 +1,30 @@
+import javax.persistence.*;
+import java.util.List;
 import java.util.Objects;
 
+@Entity
+@Table(name = "city")
 public class City {
-    private String cityName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
     private Integer cityId;
+    @Column(name = "city_name")
+    private String cityName;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Employee> employee;
 
     public City(String cityName, Integer cityId) {
         this.cityName = cityName;
         this.cityId = cityId;
+    }
+
+    public City(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public City() {
+
     }
 
     public String getCityName() {
@@ -23,6 +41,27 @@ public class City {
 
     public void setCityId(Integer cityId) {
         this.cityId = cityId;
+    }
+
+    public List<Employee> getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(List<Employee> employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return Objects.equals(cityId, city.cityId) && Objects.equals(cityName, city.cityName) && Objects.equals(employee, city.employee);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cityId, cityName, employee);
     }
 
     @Override
